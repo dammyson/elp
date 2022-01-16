27f401aa9ec4796822a12ae51d2165d05207d1d5
“dammyson” <voecmanager@yahoo.com>
2022-01-16 11:58:06 -0600

    nww

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckCompany::class,
-            \App\Http\Middleware\HandleInertiaRequests::class,
+          
 
         ],
 
